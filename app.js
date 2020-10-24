a81a1b39cff96e3c0370fdda60d9784cffff04a0
PrimeLeon <PrimeLeon@qq.com>
2020-10-24 15:57:07 +0800

    others(*):add other files.

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = require('./router');
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+app.use('/', router);
+
+const server = app.listen(5000, () => {
+    const {
+        address,
+        port
+    } = server.address();
+    console.log(`Http server activated ${address}:${port}`);
+})
